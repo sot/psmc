96ae03691f1a33aa797b6b8ecafab55d44bd3f21
jeanconn <devnull@localhost>
2009-09-05 13:54:10 +0000

    Added task schedule and such to project

--- predict/run_psmc_daily.py
+++ predict/run_psmc_daily.py
@@ -37,13 +37,6 @@ def get_options():
     parser.add_option("--data_dir",
                       default=TASK_DATA,
                       help="parent directory for data by year/day")
-    parser.add_option("--traceback",
-                      default=True,
-                      help='Enable tracebacks')
-    parser.add_option("--verbose",
-                      type='int',
-                      default=1,
-                      help="Verbosity (0=quiet, 1=normal, 2=debug)")
     opt, args = parser.parse_args()
     return opt, args
 
@@ -54,42 +47,12 @@ def main(opt):
     day_dir = os.path.join( opt.data_dir, "%s" % run_time_mx.year, "%s" % run_time_mx.day_of_year )
     if not os.path.exists(day_dir):
         os.makedirs(day_dir)
+    print PSMC_CHECK_EXE
     os.system("%s --run_start_time %s --days %s --outdir %s" 
               % ( PSMC_CHECK_EXE, run_time_date, opt.telem_days, day_dir))
 
 
-#    if not os.path.exists(opt.outdir):
-#        os.mkdir(opt.outdir)
-#
-#    config_logging(opt.outdir, opt.verbose)
-
-
-def config_logging(outdir, verbose):
-    """Set up file and console logging.
-    See http://docs.python.org/library/logging.html#logging-to-multiple-destinations
-    """
-    loglevel = { 0: logging.CRITICAL,
-                 1: logging.INFO,
-                 2: logging.DEBUG }.get(verbose, logging.INFO)
-    logging.basicConfig(level=loglevel,
-                        format='%(message)s',
-                        filename=os.path.join(outdir, 'run.dat'),
-                        filemode='w')
-
-    console = logging.StreamHandler()
-    console.setLevel(loglevel)
-    formatter = logging.Formatter('%(message)s')
-    console.setFormatter(formatter)
-    logging.getLogger('').addHandler(console)
-
-
 if __name__ == '__main__':
+
     opt, args = get_options()
-    try:
-        main(opt)
-    except Exception, msg:
-        if opt.traceback:
-            raise
-        else:
-            print "ERROR:", msg
-            sys.exit(1)
+    main(opt)
